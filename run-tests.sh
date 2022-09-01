c70b9e9e70e6f9635d4a60cac2e60197a1e2d641
Schparky <3172830+Schparky@users.noreply.github.com>
2022-09-01 17:22:26 +0800

    hoping a bit of a nap will help

--- run-tests.sh
+++ run-tests.sh
@@ -4,4 +4,7 @@ set -ex
 
 go install "github.com/cucumber/godog/cmd/godog@latest"
 
+# make sure all containers are ready -- just seeing if this works, there are better ways to wait
+sleep 30
+
 go test
