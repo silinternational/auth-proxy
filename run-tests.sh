eecca61ae16cb6f6a5213739a81ff8c93bff68ed
briskt <3172830+briskt@users.noreply.github.com>
2024-01-10 16:07:17 -0700

    fix: test failure wasn't stopping CI build

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,7 @@
 RED='\033[0;31m'
 RESET='\033[0m'
 
-go -v test ./...
+go test -v ./...
 
 if [ $? -ne 0 ]; then
   echo -e "\n${RED}One or more tests failed. If this is unexpected, it may be because the test server was still building.${RESET}"
