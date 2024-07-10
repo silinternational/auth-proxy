33a505a4ea4f7e0007901fcbd6a4826d522d1fac
briskt <3172830+briskt@users.noreply.github.com>
2024-07-10 08:58:30 -0600

    exit run-tests.sh with error code from go test

--- run-tests.sh
+++ run-tests.sh
@@ -3,8 +3,13 @@
 RED='\033[0;31m'
 RESET='\033[0m'
 
+# FIXME: need to wait for the `testapp` container to be ready for connections
+
 go test -v ./...
 
 if [ $? -ne 0 ]; then
   echo -e "\n${RED}One or more tests failed. If this is unexpected, it may be because the test server was still building.${RESET}"
 fi
+
+# exit the script with the exit code from `go test`
+exit $?
