f4fa5ebd35b4961284ee7f1a4945dabf31c98981
Jason Jackson <35783387+jason-jackson@users.noreply.github.com>
2024-07-10 11:06:58 -0400

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -7,7 +7,7 @@ RESET='\033[0m'
 
 go test -v ./...
 
-exit_code = $?
+exit_code=$?
 
 if [ $exit_code -ne 0 ]; then
   echo -e "\n${RED}One or more tests failed. If this is unexpected, it may be because the test server was still building.${RESET}"
